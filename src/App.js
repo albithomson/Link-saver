e43e40102d3f99e62004a2dbf5a972b259719871
albithomson <albithomson@gmail.com>
2022-09-22 00:22:30 +0530

    Home page created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 
 import './App.css';
 import Cards from './components/Cards/Cards';
-import Login from './components/Login/Login';
+// import Login from './components/Login/Login';
 
 
 function App() {
   return (
     <div className="App">
      
-      <Login/>
+      {/* <Login/> */}
       <Cards/>
     </div>
   );
--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,10 +1,49 @@
 import React from 'react'
 import logo from "./Logo.svg";
+import "./Cards.css";
+import add from './Add.svg';
 
 const Cards = () => {
   return (
-<div>
-    hello
+<div className='container'>
+   <div className='header'>
+    <img className='logo' src={logo}></img>
+    <h1 className='text'>LINKSAVER</h1>
+ </div>
+
+<div className='data'>
+ <div className='form'>
+  <form>
+    <div><input className='form-input' type="text" placeholder='Enter info here'></input></div>
+    <div><input className='form-input2' type="text" placeholder='Paste link here'></input></div>
+   <div className='sub-btn'><button>Submit</button></div> 
+  </form>
+  </div>
+
+  <div className='form'>
+  <form>
+    <div><input className='form-input' type="text" placeholder='Enter info here'></input></div>
+    <div><input className='form-input2' type="text" placeholder='Paste link here'></input></div>
+   <div className='sub-btn'><button>Submit</button></div> 
+  </form>
+  </div>
+
+ <div className='form'>
+  <form>
+    <div><input className='form-input' type="text" placeholder='Enter info here'></input></div>
+    <div><input className='form-input2' type="text" placeholder='Paste link here'></input></div>
+   <div className='sub-btn'><button>Submit</button></div> 
+  </form>
+</div>
+
+<div className='form'>
+  
+  <form>
+    <img className='add' src={add}></img>
+    <span className='add-text'>Add new link</span>
+  </form>
+  </div>
+ </div>
 </div>
   )
 }
